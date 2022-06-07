75dd30a69446733b48ba3a0b8bfe75218d7e3fc1
Jayanthi Ramalingam <jayanthir@stg.com>
2022-06-07 12:20:48 +0530

    mess

---
+++ src/main/java/com/stgi/stginfo/StginfoApplication.java
@@ -0,0 +1,16 @@
+package com.stgi.stginfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StginfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StginfoApplication.class, args);
+	}
+
+}
+
+
+
